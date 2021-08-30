78b45bbc73ba10a6ca8253517aedd1de9be306b4
valkyrienyanko <sebastianbelle074@protonmail.com>
2021-08-30 15:07:46 -0500

    UI Game Menu Navigation

--- Scripts/UI/UIGameMenu.cs
+++ Scripts/UI/UIGameMenu.cs
@@ -1,20 +1,71 @@
+using System.Collections.Generic;
 using Godot;
-using System;
 using KRU.Networking;
 
-namespace KRU.UI 
+namespace KRU.UI
 {
     public class UIGameMenu : Control
     {
+        [Export] private NodePath nodePathSectionNav, nodePathSectionOptions;
+        private Dictionary<string, Control> controlSectionsMenu = new Dictionary<string, Control>();
+
+        public override void _Ready()
+        {
+            // Menu Sections
+            controlSectionsMenu["Nav"] = GetNode<Control>(nodePathSectionNav);
+            controlSectionsMenu["Options"] = GetNode<Control>(nodePathSectionOptions);
+
+            // Game Menu Parent
+            this.Visible = false;
+        }
+
+        public override void _UnhandledInput(InputEvent @event)
+        {
+            if (@event is InputEventKey eventKey)
+                if (eventKey.Pressed && eventKey.Scancode == (int)KeyList.Escape)
+                {
+                    // User is in options
+                    if (controlSectionsMenu["Options"].Visible)
+                    {
+                        ShowMenuSection("Nav");
+                    }
+                    else
+                    {
+                        // User is in nav
+                        if (this.Visible)
+                        {
+                            this.Visible = false;
+                        }
+                        else
+                        {
+                            ShowMenuSection("Nav");
+                            this.Visible = true;
+                        }
+                    }
+                }
+        }
+
         private void _on_Btn_Options_pressed()
         {
-            GetTree().ChangeScene("res://Scenes/SceneOptions.tscn");
+            ShowMenuSection("Options");
         }
 
         private void _on_Btn_Disconnect_pressed()
         {
             ENetClient.ENetCmds.Enqueue(ENetInstructionOpcode.Disconnect);
             GetTree().ChangeScene("res://Scenes/SceneMainMenu.tscn");
+        }
+
+        private void ShowMenuSection(string name)
+        {
+            HideAllMenuSections();
+            controlSectionsMenu[name].Visible = true;
+        }
+
+        private void HideAllMenuSections()
+        {
+            foreach (var section in controlSectionsMenu.Values)
+                section.Visible = false;
         }
     }
 }

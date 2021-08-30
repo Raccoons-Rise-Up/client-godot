cb4b7baf709bdae27492b2926aa09bf04ec2450f
NebulousGlitch <65990273+NebulousGlitch@users.noreply.github.com>
2021-08-29 22:58:02 -0400

    Fix because github bad
    problem with replacing files, so i had to go in godot and edit the ui again

--- Scripts/UI/UIGame.cs
+++ Scripts/UI/UIGame.cs
@@ -3,34 +3,34 @@ using System;
 
 namespace KRU.UI
 {
-    public class UIGame : Node
-    {
-        [Export] public NodePath NodePathGameMenu { get; set; }
+	public class UIGame : Node
+	{
+		[Export] public NodePath NodePathGameMenu { get; set; }
 
-        private Control controlGameMenu;
+		private Control controlGameMenu;
 
-        public override void _Ready()
-        {
-            controlGameMenu = GetNode<Control>(NodePathGameMenu);
-            controlGameMenu.Visible = Global.GameMenuOptionsActive;
-        }
+		public override void _Ready()
+		{
+			controlGameMenu = GetNode<Control>(NodePathGameMenu);
+			controlGameMenu.Visible = Global.GameMenuOptionsActive;
+		}
 
-        public override void _UnhandledInput(InputEvent @event)
-        {
-            if (@event is InputEventKey eventKey)
-                if (eventKey.Pressed && eventKey.Scancode == (int)KeyList.Escape)
-                {
-                    if (Global.GameMenuOptionsActive)
-                    {
-                        Global.GameMenuOptionsActive = false;
-                        controlGameMenu.Visible = false;
-                    }
-                    else
-                    {
-                        Global.GameMenuOptionsActive = true;
-                        controlGameMenu.Visible = true;
-                    }
-                }
-        }
-    }
+		public override void _UnhandledInput(InputEvent @event)
+		{
+			if (@event is InputEventKey eventKey)
+				if (eventKey.Pressed && eventKey.Scancode == (int)KeyList.Escape)
+				{
+					if (Global.GameMenuOptionsActive)
+					{
+						Global.GameMenuOptionsActive = false;
+						controlGameMenu.Visible = false;
+					}
+					else
+					{
+						Global.GameMenuOptionsActive = true;
+						controlGameMenu.Visible = true;
+					}
+				}
+		}
+	}
 }
--- Scripts/UI/UIMainMenu.cs
+++ Scripts/UI/UIMainMenu.cs
@@ -3,27 +3,27 @@ using System;
 
 namespace KRU.UI
 {
-    public class UIMainMenu : Node
-    {
-        private void _on_Btn_Multiplayer_pressed()
-        {
-            GetTree().ChangeScene("res://Scenes/SceneLogin.tscn");
-        }
+	public class UIMainMenu : Node
+	{
+		private void _on_Btn_Multiplayer_pressed()
+		{
+			GetTree().ChangeScene("res://Scenes/SceneLogin.tscn");
+		}
 
-        private void _on_Btn_Options_pressed()
-        {
-            GetTree().ChangeScene("res://Scenes/SceneOptions.tscn");
-        }
+		private void _on_Btn_Options_pressed()
+		{
+			GetTree().ChangeScene("res://Scenes/SceneOptions.tscn");
+		}
 
-        private void _on_Btn_Credits_pressed()
-        {
-            GetTree().ChangeScene("res://Scenes/SceneCredits.tscn");
-        }
+		private void _on_Btn_Credits_pressed()
+		{
+			GetTree().ChangeScene("res://Scenes/SceneCredits.tscn");
+		}
 
-        private void _on_Btn_Quit_pressed()
-        {
-            GetTree().Quit();
-        }
-    }
+		private void _on_Btn_Quit_pressed()
+		{
+			GetTree().Quit();
+		}
+	}
 }
 

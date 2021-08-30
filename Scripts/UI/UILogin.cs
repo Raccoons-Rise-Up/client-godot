b9c08cca87e18dac2d03d5a3484f87cc425da294
valkyrienyanko <sebastianbelle074@protonmail.com>
2021-08-30 15:08:15 -0500

    Cleanup

--- Scripts/UI/UILogin.cs
+++ Scripts/UI/UILogin.cs
@@ -4,20 +4,13 @@ using KRU.Networking;
 
 namespace KRU.UI
 {
-    public class UILogin : Node
+    public class UILogin : Control
     {
         private ENetClient ENetClient;
 
         public override void _Ready()
         {
             ENetClient = GetNode<ENetClient>("/root/ENetClient");
-        }
-
-        public override void _UnhandledInput(InputEvent @event)
-        {
-            if (@event is InputEventKey eventKey)
-                if (eventKey.Pressed && eventKey.Scancode == (int)KeyList.Escape)
-                    GetTree().ChangeScene("res://Scenes/SceneMainMenu.tscn");
         }
 
         private void _on_Btn_Connect_pressed()

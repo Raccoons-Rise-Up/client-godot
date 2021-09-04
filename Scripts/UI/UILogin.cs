8dcd3657a6fdc482a53ecf2633a2d02a0d1c4545
valkyrienyanko <sebastianbelle074@protonmail.com>
2021-09-04 17:59:59 -0500

    System.Net.Http

--- Scripts/UI/UILogin.cs
+++ Scripts/UI/UILogin.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Threading.Tasks;
 using System.IO;
+using System.Text;
+using System.Net.Http;
 using Godot;
 using Godot.Collections;
 using Newtonsoft.Json;
 using KRU.Networking;
 using KRU.IO;
+
+using HttpClient = System.Net.Http.HttpClient;
 
 namespace KRU.UI
 {
@@ -20,7 +26,7 @@ namespace KRU.UI
         private static Label loginAsSection, labelResponse;
         private static VBoxContainer loginSection;
         private static ENetClient ENetClient;
-        private static Dictionary<string, string> theContents; // token contents (messy, clean up later..)
+        private static string Token { get; set; }
 
         public override void _Ready()
         {
@@ -60,7 +66,7 @@ namespace KRU.UI
             UpdateResponse("");
 
             var contents = AppData.GetJsonWebToken();
-            if (contents == null || contents["token"] == "")
+            if (contents == null || contents["token"] == null)
             {
                 HideConnectAsSection();
             }
@@ -79,42 +85,118 @@ namespace KRU.UI
         private static void Logout()
         {
             UpdateResponse("");
-            AppData.SaveJsonWebToken("", "");
-            theContents = null;
+            AppData.SaveJsonWebToken(null, "");
+            Token = null;
             HideConnectAsSection();
         }
 
-        private void _on_Btn_Login_pressed()
-        {
-            var contents = AppData.GetJsonWebToken();
+        private static readonly HttpClient webClient = new HttpClient();
+        private static bool sendingRequest;
 
-            Dictionary<string, string> data_to_send;
-            if (contents == null || contents["token"] == "")
+        private static async Task<string> GetRequest(string path)
+        {
+            if (sendingRequest)
+                return "";
+
+            sendingRequest = true;
+
+            string stringTask = "";
+            try
             {
-                data_to_send = new Dictionary<string, string>{
-                    { "username", inputUsername.Text },
-                    { "password", inputPassword.Text },
-                    { "from", "Godot-Client"}
+                stringTask = await webClient.GetStringAsync("http://127.0.0.1:4000/" + path);
+            }
+            catch (Exception e)
+            {
+                UpdateResponse(e.Message);
+            }
+
+            sendingRequest = false;
+
+            return stringTask;
+        }
+
+        private static async Task<string> PostRequest(string path, string content)
+        {
+            if (sendingRequest)
+                return "";
+
+            sendingRequest = true;
+
+            string stringTask = "";
+            using (var requestContent = new StringContent(content, Encoding.UTF8, "application/json"))
+            {
+                try
+                {
+                    using (var response = await webClient.PostAsync("http://127.0.0.1:4000/" + path, requestContent))
+                    {
+                        stringTask = await response.Content.ReadAsStringAsync();
+                    };
+                }
+                catch (Exception e)
+                {
+                    if (e is HttpRequestException)
+                        UpdateResponse("Web server is offline");
+                    else
+                        UpdateResponse(e.GetType().ToString());
+                }
+            }
+
+            sendingRequest = false;
+
+            return stringTask;
+        }
+
+        private async void _on_Btn_Login_pressed()
+        {
+            Token = AppData.GetJsonWebToken()["token"];
+
+            WebPostLoginContent loginInfo;
+            if (Token == null)
+            {
+                loginInfo = new WebPostLoginContent
+                {
+                    username = inputUsername.Text,
+                    password = inputPassword.Text,
+                    from = "Godot-Client"
                 };
             }
             else
             {
-                theContents = contents;
-                data_to_send = new Dictionary<string, string>{
-                    { "token", contents["token"] },
-                    { "from", "Godot-Client" }
+                loginInfo = new WebPostLoginContent
+                {
+                    token = Token,
+                    from = "Godot-Client"
                 };
             }
 
-            string[] headers = new string[] { "Content-Type: application/json" };
+            var jsonStr = JsonConvert.SerializeObject(loginInfo);
+            UpdateResponse("Sending login request to web server...");
+            var resStr = await PostRequest("api/login", jsonStr);
+            var res = JsonConvert.DeserializeObject<WebPostLoginResponse>(resStr);
+            UpdateResponse(res.message);
 
-            string query = JSON.Print(data_to_send);
-
-            var error = httpRequest.Request("http://127.0.0.1:4000/api/login", headers, false, HTTPClient.Method.Post, query);
-
-            if (error != Error.Ok)
+            switch ((LoginOpcode)res.opcode)
             {
-                GD.Print(error);
+                case LoginOpcode.AccountDoesNotExist:
+                case LoginOpcode.InvalidUsernameOrPassword:
+                case LoginOpcode.PasswordsDoNotMatch:
+                    break;
+                case LoginOpcode.InvalidToken:
+                    Logout();
+                    break;
+                case LoginOpcode.LoginSuccess:
+                    if (Token != null)
+                    {
+                        ENetClient.JsonWebToken = Token;
+                    }
+                    else
+                    {
+                        AppData.SaveJsonWebToken(res.token, inputUsername.Text);
+                        ENetClient.JsonWebToken = res.token;
+                    }
+
+                    ENetClient.Connect();
+                    break;
             }
         }
 
@@ -129,50 +211,14 @@ namespace KRU.UI
             loginSection.Visible = true;
             btnLogout.Visible = false;
         }
+    }
 
-        private void OnRequestCompleted(int result, int response_code, string[] headers, byte[] body)
-        {
-            if (response_code == 429)
-            {
-                UpdateResponse("Please wait a bit before making another request");
-                return;
-            }
-            if (response_code == 404)
-            {
-                UpdateResponse("Web server responded with 404"); // page not found
-                return;
-            }
-
-            var json = System.Text.Encoding.UTF8.GetString(body);
-            var res = JsonConvert.DeserializeObject<WebPostLoginResponse>(json);
-
-            switch ((LoginOpcode)res.opcode)
-            {
-                case LoginOpcode.AccountDoesNotExist:
-                case LoginOpcode.InvalidUsernameOrPassword:
-                case LoginOpcode.PasswordsDoNotMatch:
-                    UpdateResponse(res.message);
-                    break;
-                case LoginOpcode.InvalidToken:
-                    UpdateResponse(res.message);
-                    Logout();
-                    break;
-                case LoginOpcode.LoginSuccess:
-                    UpdateResponse(res.message);
-                    if (theContents != null && theContents["token"] != "")
-                    {
-                        ENetClient.JsonWebToken = theContents["token"];
-                    }
-                    else
-                    {
-                        AppData.SaveJsonWebToken(res.token, inputUsername.Text);
-                        ENetClient.JsonWebToken = res.token;
-                    }
-
-                    ENetClient.Connect();
-                    break;
-            }
-        }
+    public class WebPostLoginContent
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string token { get; set; }
+        public string from { get; set; }
     }
 
     public class WebPostLoginResponse

7c87a8cee416e027f07a8e9d5727543aafe84916
valkyrienyanko <sebastianbelle074@protonmail.com>
2021-09-08 19:28:57 -0500

    Handle TooManyRequests / Cleanup

--- Scripts/UI/UILogin.cs
+++ Scripts/UI/UILogin.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using System.IO;
 using System.Text;
+using System.Net;
 using System.Net.Http;
 using Godot;
 using Godot.Collections;
@@ -28,6 +29,9 @@ namespace KRU.UI
         private static ENetClient ENetClient;
         private static string Token { get; set; }
         private static bool AttemptedToRenewInvalidToken { get; set; }
+
+        private static readonly HttpClient webClient = new HttpClient();
+        private static bool SendingRequest { get; set; }
 
         public override void _Ready()
         {
@@ -90,15 +94,12 @@ namespace KRU.UI
             HideConnectAsSection();
         }
 
-        private static readonly HttpClient webClient = new HttpClient();
-        private static bool sendingRequest;
-
         private static async Task<string> GetRequest(string path)
         {
-            if (sendingRequest)
+            if (SendingRequest)
                 return "Currently sending a request please wait...";
 
-            sendingRequest = true;
+            SendingRequest = true;
 
             string stringTask = "";
             try
@@ -107,26 +108,22 @@ namespace KRU.UI
             }
             catch (Exception e)
             {
-                sendingRequest = false;
+                SendingRequest = false;
 
                 return e.Message;
             }
 
-            sendingRequest = false;
+            SendingRequest = false;
 
             return stringTask;
         }
 
         private static async Task<WebPostResponse> PostRequest(string path, string content)
         {
-            if (sendingRequest)
-                return new WebPostResponse
-                {
-                    Opcode = WebPostResponseOpcode.InMiddleOfRequest,
-                    Message = "Currently sending a request please wait..."
-                };
+            if (SendingRequest)
+                return WebPostResponse(WebPostResponseOpcode.InMiddleOfRequest, "Currently sending a request please wait...", false);
 
-            sendingRequest = true;
+            SendingRequest = true;
 
             string stringTask = "";
             using (var requestContent = new StringContent(content, Encoding.UTF8, "application/json"))
@@ -135,34 +132,40 @@ namespace KRU.UI
                 {
                     using (var response = await webClient.PostAsync("http://127.0.0.1:4000/" + path, requestContent))
                     {
+                        GD.Print(response.StatusCode);
+
+                        switch (response.StatusCode)
+                        {
+                            case (HttpStatusCode)429: // HttpStatusCode.TooManyRequests not supported in this version of .NET
+                                return WebPostResponse(WebPostResponseOpcode.TooManyRequests, "Please wait a bit before making another request");
+                            case HttpStatusCode.OK:
+                                break;
+                        }
+
                         stringTask = await response.Content.ReadAsStringAsync();
                     };
                 }
                 catch (Exception e)
                 {
-                    sendingRequest = false;
-
                     if (e is HttpRequestException)
-                        return new WebPostResponse
-                        {
-                            Opcode = WebPostResponseOpcode.WebServerOffline,
-                            Message = "Web server is offline"
-                        };
+                        return WebPostResponse(WebPostResponseOpcode.WebServerOffline, "Web server is offline");
                     else
-                        return new WebPostResponse
-                        {
-                            Opcode = WebPostResponseOpcode.Exception,
-                            Message = e.GetType().ToString()
-                        };
+                        return WebPostResponse(WebPostResponseOpcode.Exception, e.GetType().ToString());
                 }
             }
 
-            sendingRequest = false;
+            return WebPostResponse(WebPostResponseOpcode.Success, stringTask);
+        }
+
+        private static WebPostResponse WebPostResponse(WebPostResponseOpcode opcode, string message, bool resetSendingRequest = true)
+        {
+            if (resetSendingRequest)
+                SendingRequest = false;
 
             return new WebPostResponse
             {
-                Opcode = WebPostResponseOpcode.Success,
-                Message = stringTask
+                Opcode = opcode,
+                Message = message
             };
         }
 
@@ -198,13 +201,29 @@ namespace KRU.UI
                 case WebPostResponseOpcode.InMiddleOfRequest:
                 case WebPostResponseOpcode.WebServerOffline:
                 case WebPostResponseOpcode.Exception:
+                case WebPostResponseOpcode.TooManyRequests:
                     UpdateResponse(webResponse.Message);
                     return;
                 case WebPostResponseOpcode.Success:
                     break;
             }
 
-            var res = JsonConvert.DeserializeObject<WebPostLoginResponse>(webResponse.Message);
+
+
+            GD.Print(webResponse.Message);
+
+            WebPostLoginResponse res;
+            try
+            {
+                res = JsonConvert.DeserializeObject<WebPostLoginResponse>(webResponse.Message);
+            }
+            catch (JsonReaderException e)
+            {
+
+                GD.Print(e.Message);
+                return;
+            }
+
             UpdateResponse(res.Message);
 
             switch ((LoginOpcode)res.Opcode)
@@ -267,6 +286,7 @@ namespace KRU.UI
     {
         InMiddleOfRequest,
         WebServerOffline,
+        TooManyRequests,
         Exception,
         Success
     }

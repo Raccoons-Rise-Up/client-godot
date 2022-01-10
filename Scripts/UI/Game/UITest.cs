32dcdb66428fe0ded1016a2201296e8b042d0aa7
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-01-10 16:15:46 -0600

    Tech Tree Pan Logic Attempt

--- Scripts/UI/Game/UITest.cs
+++ Scripts/UI/Game/UITest.cs
@@ -1,34 +1,58 @@
 using Godot;
 using System;
+using Client.Utilities;
 
 public class UITest : Node
 {
 #pragma warning disable CS0649 // Values are assigned in the editor
     [Export] private readonly NodePath nodePathPanel;
+    [Export] private readonly NodePath nodePathMask;
 #pragma warning restore CS0649 // Values are assigned in the editor
 
     private static Control Panel;
+    private static Control Mask;
 
     private bool Drag { get; set; }
+    private Vector2 ClickPos = Vector2.Zero;
 
     public override void _Ready()
     {
         Panel = GetNode<Control>(nodePathPanel);
+        Mask = GetNode<Control>(nodePathMask);
     }
 
     public override void _PhysicsProcess(float delta)
     {
         if (Drag)
         {
-            Panel.RectGlobalPosition = GetViewport().GetMousePosition();
+            Panel.RectGlobalPosition = Utils.Lerp(GetViewport().GetMousePosition() - ClickPos, GetViewport().GetMousePosition() - ClickPos, 25 * delta);
         }
-    }
-    
-    Vector2 Lerp(Vector2 firstVector, Vector2 secondVector, float by)
-    {
-        float retX = Mathf.Lerp(firstVector.x, secondVector.x, by);
-        float retY = Mathf.Lerp(firstVector.y, secondVector.y, by);
-        return new Vector2(retX, retY);
+        else 
+        {
+            //if (Panel.RectGlobalPosition > Mask.RectGlobalPosition)
+                //Panel.RectGlobalPosition = Utils.Lerp(Panel.RectGlobalPosition, Mask.RectGlobalPosition, 25 * delta);
+
+            //if (Panel.RectGlobalPosition + Panel.RectSize - Mask.RectSize < Mask.RectGlobalPosition)
+                //Panel.RectGlobalPosition = Utils.Lerp(Panel.RectGlobalPosition, Mask.RectGlobalPosition + Mask.RectSize, 1 * delta);
+
+            // Content too far away from top right corner of mask
+            if (Panel.RectGlobalPosition.x + Panel.RectSize.x - Mask.RectSize.x < Mask.RectGlobalPosition.x)
+                if (Panel.RectGlobalPosition.y + Panel.RectSize.y - Mask.RectSize.y > Mask.RectGlobalPosition.y) 
+                    Panel.RectGlobalPosition = Mask.RectGlobalPosition - new Vector2(Panel.RectSize.x, 0) + new Vector2(Mask.RectSize.x, 0);
+
+            if (Panel.RectGlobalPosition.y + Panel.RectSize.y < Mask.RectGlobalPosition.y + Mask.RectSize.y)
+            {
+                var diff = Panel.RectGlobalPosition.y - Mask.RectGlobalPosition.y;
+                GD.Print(diff);
+                Panel.RectGlobalPosition = Panel.RectGlobalPosition - new Vector2(0, diff);
+                GD.Print("true");
+            } else 
+            {
+                //GD.Print("false");
+            }
+                //Panel.RectGlobalPosition = Mask.RectGlobalPosition - Panel.RectSize;
+                    
+        }
     }
 
     private void _on_PanelContainer2_gui_input(InputEvent @event)
@@ -37,6 +61,7 @@ public class UITest : Node
         {
             if (Input.IsActionJustPressed("left_click"))
             {
+                ClickPos = Panel.GetLocalMousePosition();
                 Drag = true;
             }
 
--- Scripts/Utils/Utils.cs
+++ Scripts/Utils/Utils.cs
@@ -1,3 +1,5 @@
+using Godot;
+
 namespace Client.Utilities 
 {
     public class Utils 
@@ -5,6 +7,13 @@ namespace Client.Utilities
         public static float Remap(float s, float a1, float a2, float b1, float b2)
         {
             return b1 + (s-a1)*(b2-b1)/(a2-a1);
+        }
+
+        public static Vector2 Lerp(Vector2 firstVector, Vector2 secondVector, float by)
+        {
+            float retX = Mathf.Lerp(firstVector.x, secondVector.x, by);
+            float retY = Mathf.Lerp(firstVector.y, secondVector.y, by);
+            return new Vector2(retX, retY);
         }
     }
 }

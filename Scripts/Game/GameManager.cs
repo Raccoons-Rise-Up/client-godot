436957dc0778ede4b978a3741b0b624f75e05097
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-03-23 11:00:57 -0500

    Remove 3D World Stuff

--- Scripts/Game/GameManager.cs
+++ Scripts/Game/GameManager.cs
@@ -36,13 +36,6 @@ namespace Client.Game
         {
             UIOptions.Save();
 
-            var chunkTimer = World.ChunkTimer;
-            if (chunkTimer != null)
-            {
-                chunkTimer.Enabled = false;
-                chunkTimer.Dispose();
-            }
-
             if (ENetClient.ENetThreadRunning)
             {
                 Tree.SetAutoAcceptQuit(false);

9be32a208215a89ad3807806240e3d8dd8963363
valkyrienyanko <sebastianbelle074@protonmail.com>
2022-03-24 14:22:58 -0500

    Position Last 2 Columns

--- Scripts/UI/UITechTreeResearch.cs
+++ Scripts/UI/UITechTreeResearch.cs
@@ -165,13 +165,16 @@ namespace Client.UI
 
             SortNodesByDepth();
 
+            // --- DEBUG ---
             foreach (var node in Nodes)
             {
                 var depth = node.Key;
                 var children = node.Value.Select(x => x.ToString()).ToArray();
 
+                // Print nodes sorted by depth
                 GD.Print($"{depth} {string.Join(" ", children)}");
             }
+            // --- DEBUG ---
 
             PositionNodes();
         }
@@ -192,7 +195,63 @@ namespace Client.UI
 
         private static void PositionNodes()
         {
-            
+            // Step 1
+            // Place nodes at MaxDepth
+            {
+                var y = 0;
+                foreach (var node in Nodes[MaxDepth])
+                {
+                    ResearchData[node].Position = new Vector2(600, y * SPACING_V);
+                    y++;
+                    CreateNode(node);
+                }
+            }
+
+            // Step 2a
+            // Determine positions of nodes at MaxDepth - 1 based off positions of nodes at MaxDepth
+
+            // Nodes that were not positioned yet
+            var nodesWithNoChildren = new List<ResearchType>();
+            var largestYPos = 0f;
+
+            foreach (var node in Nodes[MaxDepth - 1]) 
+            {
+                var data = ResearchData[node];
+                var pos = Vector2.Zero;
+
+                if (data.Unlocks == null)
+                {
+                    // No children
+                    nodesWithNoChildren.Add(node);
+                }
+                else
+                {
+                    // Find middle y pos based off children
+                    for (int i = 0; i < data.Unlocks.Length; i++)
+                    {
+                        pos += ResearchData[data.Unlocks[i]].Position;
+                    }
+                    pos /= data.Unlocks.Length;
+
+                    data.Position = new Vector2(400, pos.y);
+
+                    // Determine largest Y pos
+                    if (pos.y > largestYPos)
+                        largestYPos = pos.y;
+
+                    CreateNode(node);
+                }
+            }
+
+            // Step 2b
+            for (int i = 0; i < nodesWithNoChildren.Count; i++)
+            {
+                var data = ResearchData[nodesWithNoChildren[i]];
+
+                data.Position = new Vector2(400, largestYPos + (i + 1) * SPACING_V);
+
+                CreateNode(nodesWithNoChildren[i]);
+            }
         }
 
         public static void CreateNode(ResearchType type)
